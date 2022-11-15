1fb70e78b485771c3e1d470494a470800d208056
munkerrrrr <migos1628@gmail.com>
2022-11-15 15:55:02 +0800

    gah

--- social/src/App.js
+++ social/src/App.js
@@ -1,10 +1,20 @@
-
+import { sendRequest, urlLookup } from "./settings/settings";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div>
-      hello
-    </div>;
-  
-}
+  const [datas, setDatas] = useState();
+  useEffect(() => {
+    const bodyChiglel = {
+      action: "chiglel",
+    };
 
+    sendRequest(urlLookup, bodyChiglel).then((data) => setDatas(data));
+  }, []);
+
+  return (
+    <>
+      <p>{datas && JSON.stringify(datas)}</p>
+    </>
+  );
+}
 export default App;
